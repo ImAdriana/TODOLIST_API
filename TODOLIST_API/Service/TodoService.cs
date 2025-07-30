3211921980e3e15823005cd3099db514182cad13
Im Adriana Clemente <clemente.hernandez.alicia.adriana@gmail.com>
2025-07-29 19:44:26 -0600

    Implementación del Mapeo y Repositorio

--- TODOLIST_API/Service/TodoService.cs
+++ TODOLIST_API/Service/TodoService.cs
@@ -1,138 +1,86 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TODOLIST_API.Context;
 using TODOLIST_API.DTO;
+using TODOLIST_API.Mapping;
 using TODOLIST_API.Models;
+using TODOLIST_API.Repository;
 
 namespace TODOLIST_API.Service
 {
     public class TodoService : ITodoService
     {
-        AppDbContext _context;
-        public TodoService(AppDbContext context)
+        private ITodoRepository<TodoItem> _todoRepository;
+        private IMapper _mapper;
+        public TodoService( ITodoRepository<TodoItem> todoRepository,
+            IMapper mapper)
         {
-            _context = context;
+            _todoRepository = todoRepository;
+            _mapper = mapper;
         }
 
-        public async Task<IEnumerable<TodoDto>> Get() =>
-           await _context.TodoItems.Select(t => new TodoDto
-            {
-                Id = t.Id,
-                Title = t.Title,
-                DescriptionItem = t.DescriptionItem,
-                CreatedDate = t.CreatedDate,
-                EndedDate = t.EndedDate,
-                isCompleted = t.isCompleted,
+        public async Task<IEnumerable<TodoDto>> Get() {
+            var item = await _todoRepository.GetAll();
+            return item.Select(m => _mapper.Map<TodoDto>(m));
 
-            }).ToListAsync();
-
+        }
         
         public async Task<TodoDto> GetById(int id)
         {
-            var item = await _context.TodoItems.FindAsync(id);
+            var item = await _todoRepository.GetById(id);
             if (item != null)
             {
-                var todoDto = new TodoDto
-                {
-                    Id = item.Id,
-                    Title = item.Title,
-                    DescriptionItem = item.DescriptionItem,
-                    CreatedDate = item.CreatedDate,
-                    EndedDate = item.EndedDate,
-                    isCompleted = item.isCompleted,
-                };
+                var todoDto = _mapper.Map<TodoDto>(item);
                 return todoDto;
             }
             return null;
         }
         public async Task<IEnumerable<TodoDto>> GetItemIsCompleted(bool isCompleted)
         {
-            var item = await _context.TodoItems
-                .Where(t => t.isCompleted == isCompleted)
-                .Select(t => new TodoDto
-                {
-                    Id = t.Id,
-                    Title = t.Title,
-                    DescriptionItem = t.DescriptionItem,
-                    CreatedDate = t.CreatedDate,
-                    EndedDate = t.EndedDate,
-                    isCompleted = t.isCompleted
-                })
-                .ToListAsync();
-            return item != null ? item : null;
-
+            var items = await _todoRepository.IsTaskCompleted(isCompleted);
+            if (items != null)
+            {
+                return items.Select(m => _mapper.Map<TodoDto>(m)); 
+            }
+            return null;
         }
 
         public async Task<TodoDto> InsertItem(TodoInsertDto todoInsertDto)
         {
-            var item = new TodoItem
-            {
-                Title = todoInsertDto.Title,
-                DescriptionItem = todoInsertDto.DescriptionItem,
-                CreatedDate = todoInsertDto.CreatedDate,
-                EndedDate = todoInsertDto.EndedDate,
-                isCompleted = todoInsertDto.isCompleted,
-            };
+            var item = _mapper.Map<TodoItem>(todoInsertDto);
 
-            await _context.TodoItems.AddAsync(item);
-            await _context.SaveChangesAsync();
+            await _todoRepository.Add(item);
+            await _todoRepository.Save();
 
-            var itemDto = new TodoDto
-            {
-                Id = item.Id,
-                Title = item.Title,
-                DescriptionItem = item.DescriptionItem,
-                CreatedDate = item.CreatedDate,
-                EndedDate = item.EndedDate,
-                isCompleted = item.isCompleted
-            };
+            var itemDto = _mapper.Map<TodoDto>(item);
 
             return itemDto;
         }
 
         public async Task<TodoDto> UpdateItem(int id, TodoUpdateDto todoUpdateDto)
         {
-            var item = await _context.TodoItems.FindAsync(id);
+            var item = await _todoRepository.GetById(id);
             if (item != null)
             {
-                item.Id = todoUpdateDto.Id;
-                item.Title = todoUpdateDto.Title;
-                item.DescriptionItem = todoUpdateDto.DescriptionItem;
-                item.CreatedDate = todoUpdateDto.CreatedDate;
-                item.EndedDate = todoUpdateDto.EndedDate;
-                item.isCompleted = todoUpdateDto.isCompleted;
+                item = _mapper.Map<TodoUpdateDto, TodoItem>(todoUpdateDto, item);
 
-                await _context.SaveChangesAsync();
+                await _todoRepository.Save();
 
-                var itemDto = new TodoDto
-                {
-                    Id = item.Id,
-                    Title = item.Title,
-                    DescriptionItem = item.DescriptionItem,
-                    CreatedDate = item.CreatedDate,
-                    EndedDate = item.EndedDate,
-                    isCompleted = item.isCompleted
-                };
+                var itemDto = _mapper.Map<TodoDto>(item);
                 return itemDto;
             }
             return null;
         }
         public async Task<TodoDto> DeleteItem(int id)
         {
-            var item = await _context.TodoItems.FindAsync(id);
+            var item = await _todoRepository.GetById(id);
             if (item != null)
             {
-                var itemDto = new TodoDto
-                {
-                    Id = item.Id,
-                    Title = item.Title,
-                    DescriptionItem = item.DescriptionItem,
-                    CreatedDate = item.CreatedDate,
-                    EndedDate = item.EndedDate,
-                    isCompleted = item.isCompleted
-                };
-                _context.Remove(item);
-                await _context.SaveChangesAsync();
+                var itemDto = _mapper.Map<TodoDto>(item);
+
+                _todoRepository.Delete(item);
+                await _todoRepository.Save();
                 return itemDto;
             }
             return null;
