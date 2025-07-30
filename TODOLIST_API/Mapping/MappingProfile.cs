8f54ec56011b4b6c3d3609848fc65d6dc8c91a14
Im Adriana Clemente <clemente.hernandez.alicia.adriana@gmail.com>
2025-07-29 19:42:34 -0600

    Clase que declara el Mapeo de objetos

---
+++ TODOLIST_API/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TODOLIST_API.DTO;
+using TODOLIST_API.Models;
+
+namespace TODOLIST_API.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<TodoItem, TodoDto>();
+            CreateMap<TodoInsertDto, TodoItem>();
+            CreateMap<TodoUpdateDto, TodoItem>();
+        }
+    }
+}
