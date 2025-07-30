191d04fe406c27d47dca2eba0717619c50ff7ace
Im Adriana Clemente <clemente.hernandez.alicia.adriana@gmail.com>
2025-07-29 19:43:19 -0600

    Repositorio que se conecta con la BD

---
+++ TODOLIST_API/Repository/ITodoRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace TODOLIST_API.Repository
+{
+    public interface ITodoRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(int id);
+
+        Task<IEnumerable<TEntity>> IsTaskCompleted(bool isCompleted);
+        Task Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task Save();
+
+    }
+}
---
+++ TODOLIST_API/Repository/TodoRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TODOLIST_API.Context;
+using TODOLIST_API.Models;
+
+namespace TODOLIST_API.Repository
+{
+    public class TodoRepository : ITodoRepository<TodoItem>
+    {
+        private AppDbContext _context;
+        public TodoRepository( AppDbContext context) { 
+            _context = context;
+        }
+        public async Task<IEnumerable<TodoItem>> GetAll()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        public async Task<TodoItem> GetById(int id)
+        {
+            return await _context.TodoItems.FindAsync(id);
+
+        }
+        public async Task<IEnumerable<TodoItem>> IsTaskCompleted(bool isCompleted)
+        {
+            return await _context.TodoItems.Where(t => t.isCompleted == isCompleted).ToListAsync();
+        }
+        public async Task Add(TodoItem entity)
+        
+            => await _context.TodoItems.AddAsync(entity);
+
+        public async Task Save()
+            => await _context.SaveChangesAsync();
+
+        public void Update(TodoItem entity)
+        {
+           _context.TodoItems.Attach(entity);
+            _context.TodoItems.Entry(entity).State = EntityState.Modified;
+        }
+        public void Delete(TodoItem entity)
+
+            => _context.TodoItems.Remove(entity);
+
+        
+    }
+}
